85ab3d9f0c522c355d348144a6ce96a527253856
Dean Poulos <dean.poulos7@gmail.com>
2020-07-17 15:13:09 +1000

    Transform into any time-interval Plotter class.

--- Plotter.py
+++ Plotter.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('BTCUSDT-1d-data.csv')
-plt.plot(data['timestamp'], data['high'])
-plt.plot(data['timestamp'], data['low'])
-plt.show()
+class Plotter:
+    def __init__(self, time):
+        self.data = pd.read_csv('BTCUSDT-' + time + '-data.csv')
 
+    def plot_candles():
+        plt.plot(data['timestamp'], data['high'])
+        plt.plot(data['timestamp'], data['low'])
+        plt.show()
